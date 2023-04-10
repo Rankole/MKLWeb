11f19f932c9fcf7ee706dba401dfa43d1b748b48
rankole <1397473667@qq.com>
2023-04-10 13:32:35 +0800

    Initial commit

---
+++ simple/src/main/java/com/izejs/simple/dto/UserDTO.java
@@ -0,0 +1,22 @@
+package com.izejs.simple.dto;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author KunKa
+ * @since 2021-04-03
+ */
+ public class UserDTO{
+    private String userName;
+
+    public String getUserName(){
+        return this.userName;
+    }
+
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+ }
+
